f26a1010a42463df70016abd51f99a6450ad070d
ITE-SSALES\ssales <salesvila67@gmail.com>
2017-09-20 10:25:03 +0200

    gg

--- src/main/java/login/UserRepository.java
+++ src/main/java/login/UserRepository.java
@@ -1,10 +1,9 @@
 package login;
 
+import login.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 // import org.springframework.stereotype.Repository;
-
-import login.User;
 
 @RepositoryRestResource(exported = false)
 public interface UserRepository extends JpaRepository<User, Long> {
