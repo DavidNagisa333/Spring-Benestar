562ac140c56ac22ff6183b88a4ff8d9ac4761b92
ITE-SSALES\ssales <salesvila67@gmail.com>
2017-10-17 11:31:37 +0200

    no message

--- src/main/java/org/benestar/classes/Oficina.java
+++ src/main/java/org/benestar/classes/Oficina.java
@@ -1,9 +1,12 @@
 package org.benestar.classes;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "oficina") // This tells Hibernate to make a table out of this class
